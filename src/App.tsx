1a1c7f79a0952d0074351f34a270960944994af7
[dyad] <sharif.alshawish1978@gmail.com>
2025-08-13 23:56:59 +0300

    Reverted all changes back to version f56a8e59cc4cb1490cf56d3f0979ad8e11b9f775

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import ExplorePage from "./pages/ExplorePage"; // Import the new ExplorePage
 
 const queryClient = new QueryClient();
 
@@ -17,7 +16,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/explore" element={<ExplorePage />} /> {/* ADD NEW ROUTE HERE */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
